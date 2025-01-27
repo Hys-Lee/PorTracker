499948638e8462dcbbea9c3c90ca31ae0d3e3024
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-01-27 16:03:37 +0900

    feat: ui ORGANISM - 메모 타일 제작작

---
+++ apps/client-vite/src/widgets/flows/ui/ORGANISMS/MemoTile.stories.tsx
@@ -0,0 +1,27 @@
+import { FormProvider, useForm } from 'react-hook-form';
+import MemoTile from './MemoTile';
+
+export default {
+  component: MemoTile,
+  title: 'MemoTile',
+  tags: ['autodocs'],
+  //👇 "Data"로 끝나는 export들은 스토리가 아닙니다.
+  excludeStories: /.*Data$/,
+  // args: {  },
+};
+
+const Template = (args) => {
+  const methods = useForm();
+  return (
+    <FormProvider {...methods}>
+      <div
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <MemoTile {...args} />
+      </div>
+    </FormProvider>
+  );
+};
+export const Default = Template.bind({});
---
+++ apps/client-vite/src/widgets/flows/ui/ORGANISMS/MemoTile.tsx
@@ -0,0 +1,75 @@
+import Text from 'src/shared/ui/ATOMS/Text';
+import CompoundForm from 'src/shared/ui/MOLECULES/CompoundForm/CompoundForm';
+import CompoundSegmentControl from 'src/shared/ui/MOLECULES/CompoundSegmentControl/CompoundSegmentControl';
+import Tile from 'src/shared/ui/MOLECULES/Tile';
+import { Controller, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const memoSchema = z.object({
+  title: z.string().nonempty(),
+  date:
+    // z.string().nonempty()
+    z.date(),
+  price: z.number().positive(),
+  currency: z.number().optional(),
+  shares: z.number().positive(), // 주식 수량
+});
+type Memo = z.infer<typeof memoSchema>;
+
+const MemoTile = () => {
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Memo>({
+    resolver: zodResolver(memoSchema),
+    defaultValues: {},
+  });
+
+  // 이 안에서 데이터 가져와야 할 듯. id같은것만 뭐 상태관리든 props든으로 가져와서
+  return (
+    <>
+      <Tile>
+        <Text as="h2" textContent={'메모'} />
+        <CompoundForm
+          onSubmit={handleSubmit((data) => {
+            console.log('제출: ', data);
+          })}
+        >
+          <CompoundForm.Input placeholder="제목" />
+          <div>
+            <CompoundForm.Label textContent="날짜" />
+            <Controller
+              name="date"
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <CompoundForm.Date
+                  onChange={(date) => {
+                    onChange(date);
+                  }}
+                  selected={value}
+                  onBlur={onBlur}
+                />
+              )}
+            />
+
+            {/* <CompoundForm.Date selected={new Date(Date.now())} /> */}
+          </div>
+          <CompoundForm.InputArea value="" placeholder="본문" />
+          <CompoundForm.Tags isLoading={false} />
+          <CompoundForm.Rating
+            maxRate={5}
+            rate={3}
+            onClick={() => {
+              console.log('레이팅 클릭');
+            }}
+          />
+          <CompoundForm.Button>저장</CompoundForm.Button>
+        </CompoundForm>
+      </Tile>
+    </>
+  );
+};
+export default MemoTile;
