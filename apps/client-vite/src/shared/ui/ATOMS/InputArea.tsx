e2fa61539d7b6be38bb3d91b48fa67d643ac218d
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-02-04 19:28:43 +0900

    feat: memo페이지 쪽, form관련 react-hook-form위한 ref처리

--- apps/client-vite/src/shared/ui/ATOMS/InputArea.tsx
+++ apps/client-vite/src/shared/ui/ATOMS/InputArea.tsx
@@ -1,17 +1,20 @@
-import { FC, TextareaHTMLAttributes } from 'react';
+import { FC, forwardRef, TextareaHTMLAttributes } from 'react';
 
 type InputAreaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
 
-const InputArea: FC<InputAreaProps> = ({ ...props }) => {
-  return (
-    <textarea
-      {...props}
-      // dangerouslySetInnerHTML={{ __html: innerHTMLContent }}
-      // value={value}
-      style={{ whiteSpace: 'pre-wrap' }}
-      wrap="hard"
-    />
-  );
-};
+const InputArea: FC<InputAreaProps> = forwardRef(
+  ({ ...props }, ref: React.ForwardedRef<null>) => {
+    return (
+      <textarea
+        ref={ref}
+        {...props}
+        // dangerouslySetInnerHTML={{ __html: innerHTMLContent }}
+        // value={value}
+        style={{ whiteSpace: 'pre-wrap' }}
+        wrap="hard"
+      />
+    );
+  }
+);
 
 export default InputArea;
--- apps/client-vite/src/shared/ui/MOLECULES/CompoundForm/CompoundFormInputArea.tsx
+++ apps/client-vite/src/shared/ui/MOLECULES/CompoundForm/CompoundFormInputArea.tsx
@@ -1,21 +1,24 @@
 import { css } from '@styled-system/css';
 import InputArea from '../../ATOMS/InputArea';
-import { TextareaHTMLAttributes } from 'react';
+import { forwardRef, TextareaHTMLAttributes } from 'react';
 
 // 그냥 기본 디자인 넣기 위해
 
 type InputAreaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
-const CompoundFormInputArea = ({ ...props }: InputAreaProps) => {
-  return (
-    <>
-      <InputArea
-        // value={value}
-        className={compoundFormInputAreaStyle}
-        {...props}
-      />
-    </>
-  );
-};
+const CompoundFormInputArea = forwardRef(
+  ({ ...props }: InputAreaProps, ref: React.ForwardedRef<null>) => {
+    return (
+      <>
+        <InputArea
+          ref={ref}
+          // value={value}
+          className={compoundFormInputAreaStyle}
+          {...props}
+        />
+      </>
+    );
+  }
+);
 export default CompoundFormInputArea;
 
 const compoundFormInputAreaStyle = css({
