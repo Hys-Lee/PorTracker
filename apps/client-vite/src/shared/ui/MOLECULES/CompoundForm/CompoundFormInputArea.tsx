a5a027907d36f2c588f889fafd1aeba78f5d4ae3
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-03-02 14:48:04 +0900

    feat: ui-MOLECULE 폼 인풋영역 디자인 적용

--- apps/client-vite/src/shared/ui/MOLECULES/CompoundForm/CompoundFormInputArea.tsx
+++ apps/client-vite/src/shared/ui/MOLECULES/CompoundForm/CompoundFormInputArea.tsx
@@ -1,18 +1,22 @@
 import { css } from '@styled-system/css';
 import InputArea from '../../ATOMS/InputArea';
-import { forwardRef, TextareaHTMLAttributes } from 'react';
+import { forwardRef, TextareaHTMLAttributes, useRef } from 'react';
 
 // 그냥 기본 디자인 넣기 위해
 
 type InputAreaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
-const CompoundFormInputArea = forwardRef(
-  ({ ...props }: InputAreaProps, ref: React.ForwardedRef<null>) => {
+const CompoundFormInputArea = forwardRef<HTMLTextAreaElement, InputAreaProps>(
+  (
+    { ...props }: InputAreaProps,
+    ref: React.ForwardedRef<HTMLTextAreaElement>
+  ) => {
     return (
       <>
+        {/* <textarea ref={ref} /> */}
         <InputArea
           ref={ref}
           // value={value}
-          className={compoundFormInputAreaStyle}
+          className={InputAreaDefaultStyle}
           {...props}
         />
       </>
@@ -20,7 +24,25 @@ const CompoundFormInputArea = forwardRef(
   }
 );
 export default CompoundFormInputArea;
-
+const InputAreaDefaultStyle = css({
+  whiteSpace: 'pre-wrap',
+  boxSizing: 'content-box',
+  padding: '12px',
+  height: '100% !important',
+  width: '100%',
+  overflowY: 'auto ',
+  resize: 'none',
+  outline: '2px solid rgb(230,230,230) !important',
+  '&:hover': {
+    '&:not(:focus)': {
+      outline: '2px solid #ceeee5',
+    },
+  },
+  '&:focus': {
+    outline: '2px solid #8cdec5',
+  },
+  borderRadius: 'md',
+});
 const compoundFormInputAreaStyle = css({
   resize: 'none',
   overflowY: 'auto',
