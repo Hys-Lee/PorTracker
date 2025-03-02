70222151b18277d78d03d713530937da70a550b6
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-03-02 14:49:36 +0900

    feat: ui-MOLECULE 폼 셀렉트 디자인 적용

--- apps/client-vite/src/shared/ui/MOLECULES/CompoundForm/CompoundFormSelect.tsx
+++ apps/client-vite/src/shared/ui/MOLECULES/CompoundForm/CompoundFormSelect.tsx
@@ -1,6 +1,7 @@
 import { Options } from 'react-select';
 import AsyncCreatableSelect from 'react-select/async-creatable';
 import { forwardRef } from 'react';
+import { css } from '@styled-system/css';
 
 type ReactSelectOptions = Options<{ value: string; label: string }>;
 interface CompoundFormSelectProps {
@@ -16,12 +17,49 @@ const CompoundFormSelect = forwardRef(
     return (
       <div>
         <AsyncCreatableSelect
+          // menuIsOpen
           ref={ref}
           isClearable
           defaultOptions={defaultOptions}
           // options={options}
           // loadOptions={loadOptions}
           isLoading={isLoading}
+          classNames={{
+            indicatorSeparator: (props) => css({ width: '0px !important' }),
+            control: (props) =>
+              css({
+                border: props.isFocused
+                  ? '2px solid #77efcd !important'
+                  : '2px solid rgb(230,230,230) !important',
+                rounded: 'xl',
+                boxShadow: props.isFocused
+                  ? '0 0 0 0.5px #b2eada !important'
+                  : 'none',
+              }),
+            menu: (props) =>
+              css({
+                boxShadow: '0 4px 11px hsla(0, 0%, 0%, 0.1) !important',
+                padding: '8px',
+                background: 'white !important',
+                gap: '8px',
+                rounded: 'xl !important',
+              }),
+            menuList: (props) =>
+              css({
+                display: 'flex !important',
+                flexDirection: 'column !important',
+                gap: '8px !important',
+              }),
+            option: (props) =>
+              css({
+                rounded: 'lg',
+                color: 'black !important',
+                background: 'white !important',
+                '&:hover': {
+                  backgroundColor: 'gray.200 !important',
+                },
+              }),
+          }}
         />
       </div>
     );
