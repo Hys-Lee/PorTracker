72280067c88b517ca409a807ecf1fa651bc598d1
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-12-08 18:17:21 +0900

    fix: chromatic CI위해 스토리북 설정 절대 경로화.
    e2e테스트하는 ci과정 제거

--- apps/core/.storybook/main.ts
+++ apps/core/.storybook/main.ts
@@ -15,7 +15,10 @@ function getAbsolutePath(value: string): string {
 }
 
 const config: StorybookConfig = {
-  stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    'apps/core/**/*.mdx',
+    'apps/core/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
   addons: [
     // getAbsolutePath('@storybook/addon-essentials'),
     // getAbsolutePath('@storybook/addon-a11y'),
