807b4fb2df4a90042ff13726efaa50e033ce5e98
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-12-08 16:48:49 +0900

    스토리북 생성 및 autodocs처리

--- apps/core/.storybook/main.ts
+++ apps/core/.storybook/main.ts
@@ -20,6 +20,7 @@ const config: StorybookConfig = {
     // getAbsolutePath('@storybook/addon-essentials'),
     // getAbsolutePath('@storybook/addon-a11y'),
     getAbsolutePath('@storybook/addon-webpack5-compiler-babel'),
+    '@storybook/addon-docs',
   ],
   framework: {
     name: getAbsolutePath('@storybook/nextjs'),
