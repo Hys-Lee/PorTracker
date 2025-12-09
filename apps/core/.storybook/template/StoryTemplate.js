bd21075febe1805786c1c97c8ca075cea9b629e5
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-12-09 13:51:29 +0900

    fix: storybook nx 실행하도록 수정

---
+++ apps/core/.storybook/template/StoryTemplate.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+var stylex = _interopRequireWildcard(require("@stylexjs/stylex"));
+var _tokens = require("./tokens.stylex");
+require("../stylex_bundle.css");
+var _jsxFileName = "C:\\Users\\dltkd\\PorTracker\\PorTracker\\apps\\core\\components\\template\\StoryTemplate.tsx",
+  _this = void 0;
+function _interopRequireWildcard(e, t) { if ("function" == typeof WeakMap) var r = new WeakMap(), n = new WeakMap(); return (_interopRequireWildcard = function _interopRequireWildcard(e, t) { if (!t && e && e.__esModule) return e; var o, i, f = { __proto__: null, "default": e }; if (null === e || "object" != typeof e && "function" != typeof e) return f; if (o = t ? n : r) { if (o.has(e)) return o.get(e); o.set(e, f); } for (var _t in e) "default" !== _t && {}.hasOwnProperty.call(e, _t) && ((i = (o = Object.defineProperty) && Object.getOwnPropertyDescriptor(e, _t)) && (i.get || i.set) ? o(f, _t, i) : f[_t] = e[_t]); return f; })(e, t); }
+var __jsx = _react["default"].createElement;
+var StoryTemplate = function StoryTemplate(_ref) {
+  var xstyle = _ref.xstyle;
+  return __jsx(_react["default"].Fragment, null, __jsx("p", {
+    className: "",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15,
+      columnNumber: 7
+    }
+  }, "\uC26C\uBC14!"), __jsx("div", (0, _extends2["default"])({}, {
+    className: "x1t5t8gb x1dmp6jm"
+  }, {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16,
+      columnNumber: 7
+    }
+  }), "\uC2A4\uD1A0\uB9AC \uD15C\uD50C\uB9BF!"));
+};
+var _default = exports["default"] = StoryTemplate;
