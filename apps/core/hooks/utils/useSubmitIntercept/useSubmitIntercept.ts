e92eb0e6cd71ef11ba51823f60299725ea6d2a12
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-12-14 18:23:38 +0900

    feat: useSubmitIntercept 제작
    비제어 방식에서 form의 submit에 대해 내부 input에서 간섭해서 동작하기 위해..
    내부 복잡한 연산 delay시켜 최적화
    
    폴더 구조 변경으로 인한 처리

---
+++ apps/core/hooks/utils/useSubmitIntercept/useSubmitIntercept.ts
@@ -0,0 +1,45 @@
+import { useEffect, useRef } from 'react';
+
+/**
+ *
+ * @param submitInterceptor : 대상 input에 대한 submit 시 액션
+ * @param form : 등록할 form id
+ * @returns input ref
+ */
+const useSubmitIntercept = (
+  submitInterceptor: (inputElement: HTMLInputElement) => void,
+  form?: string
+) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const inputElement = inputRef.current;
+    if (!inputElement) return;
+
+    const targetForm = form
+      ? document.getElementById(form)
+      : inputElement.closest('form');
+
+    // form 연결 안된다면 건너뛰기
+    if (!targetForm) {
+      console.error('연결된 form이 없습니다!');
+      return;
+    }
+
+    /** 등록 */
+    const handleFormSubmit = () => {
+      submitInterceptor(inputElement);
+    };
+
+    targetForm.addEventListener('submit', handleFormSubmit);
+
+    /** 제거 */
+    return () => {
+      targetForm.removeEventListener('submit', handleFormSubmit);
+    };
+  }, [form, submitInterceptor, inputRef.current]);
+
+  return inputRef;
+};
+
+export { useSubmitIntercept };
