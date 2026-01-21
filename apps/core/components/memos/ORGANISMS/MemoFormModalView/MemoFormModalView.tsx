99234aff7e5982a41d133f95d9c0ae20fd675658
Hys-Lee <dltkdgus3275@gmail.com>
2026-01-21 19:33:20 +0900

    feat(FE-memos): MemoFormModalView 제작

---
+++ apps/core/components/memos/ORGANISMS/MemoFormModalView/MemoFormModalView.tsx
@@ -0,0 +1,145 @@
+'use client';
+
+import FormModalFrame from '@core/components/shared/ATOMS/FormModalFrame/FormModalFrame';
+import {
+  cloneElement,
+  ComponentProps,
+  ReactElement,
+  ReactNode,
+  useState,
+} from 'react';
+import * as stylex from '@stylexjs/stylex';
+
+import SegmentControl from '@core/components/shared/MOLECULES/SegmentControl/SegmentControl';
+
+interface ActualFormModalViewProps {
+  asClose?: ComponentProps<typeof FormModalFrame>['asClose'];
+  actionButton?: ReactNode;
+  formArea: ReactNode;
+  portfolioReference: ReactNode;
+  memoReference: ReactNode;
+}
+const MemoFormModalView = ({
+  asClose,
+  actionButton,
+  formArea,
+  memoReference,
+  portfolioReference,
+}: ActualFormModalViewProps) => {
+  const [referMode, setReferMode] = useState<
+    'portfolioReference' | 'memoReference'
+  >('memoReference');
+  //   const [copiedFormData] = useAtom(copiedFormDataAtom);
+  return (
+    <FormModalFrame
+      open={true}
+      asClose={asClose}
+      frameStylex={modalStyles.frame}
+    >
+      <div
+        // style={{
+        //   display: 'flex',
+        //   flexDirection: 'column',
+        //   gap: '24px',
+        //   // justifyContent: 'space-between',
+        // }}
+        {...stylex.props(modalStyles.container)}
+      >
+        <div
+          // style={{ display: 'flex', justifyContent: 'center' }}
+          {...stylex.props(modalStyles.header)}
+        >
+          <h2
+            //   style={{ margin: '10px' }}
+            {...stylex.props(modalStyles.title)}
+          >
+            {'Memos'}
+          </h2>
+        </div>
+        <section
+          //   style={{
+          //     display: 'grid',
+          //     gridTemplateColumns: '1fr 1fr',
+          //     gap: '44px',
+          //     height: '440px',
+          //   }}
+          {...stylex.props(modalStyles.content)}
+        >
+          {/* <FormArea /> */}
+          <div style={{ width: '100%' }}>
+            {
+              formArea
+
+              //   cloneElement(formArea as ReactElement, {
+              //     key: copiedFormData?.assetInfo.value,
+              //   })
+            }
+          </div>
+          <div
+            // style={{
+            //   width: '100%',
+            //   display: 'flex',
+            //   gap: '8px',
+            //   flexDirection: 'column',
+            //   justifyContent: 'start',
+            // }}
+            {...stylex.props(modalStyles.referenceArea)}
+          >
+            <SegmentControl
+              items={referModes}
+              defaultSelected={referModes[0]}
+              onChange={(selected) => {
+                setReferMode(selected.value as typeof referMode);
+              }}
+              selected={referModes.find(({ value }) => value === referMode)}
+            />
+            {referMode === 'portfolioReference'
+              ? portfolioReference
+              : memoReference}
+          </div>
+        </section>
+        {actionButton}
+      </div>
+    </FormModalFrame>
+  );
+};
+
+export default MemoFormModalView;
+
+const referModes = [
+  { value: 'memoReference', text: 'Memo Histories' },
+  { value: 'portfolioReference', text: 'Linked Portfolio' },
+];
+
+const modalStyles = stylex.create({
+  frame: {
+    backgroundColor: 'white',
+    // height: 'auto',
+    height: '600px',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '24px',
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  title: {
+    margin: '10px',
+  },
+  content: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gap: '44px',
+    height: '440px',
+  },
+  referenceArea: {
+    width: '100%',
+    display: 'flex',
+    gap: '8px',
+    flexDirection: 'column',
+    justifyContent: 'start',
+  },
+});
