8cbbb153092c0adfbdca34780de9512e02e15bf6
Hys-Lee <dltkdgus3275@gmail.com>
2026-01-27 17:46:12 +0900

    feat(FE-memos): recent 및 요청과 응답에 대한 스키마 제작

--- apps/core/schemas/features/memos/memos.schema.ts
+++ apps/core/schemas/features/memos/memos.schema.ts
@@ -45,8 +45,14 @@ export const allPortfolioDetailedListSchema = z.array(
 
 export const memoFormSchema = z.object({
   ...memoOriginSchema.shape,
+  linkedPortfolioInfo: actualPortfolioDetailedSchema.optional(),
 });
 export const memoFormListSchema = z.array(memoFormSchema);
+
+export const memoRecentSchema = memoFormSchema.omit({
+  linkedPortfolioInfo: true,
+});
+export const memoRecentListSchema = z.array(memoRecentSchema);
 
 export type ActualPortfolioDetail = z.infer<
   typeof actualPortfolioDetailedSchema
@@ -57,3 +63,61 @@ export type TargetPortfolioDetail = z.infer<
 export type AllPortfolioDetail = z.infer<typeof allPortfolioDetailedSchema>;
 
 export type MemoForm = z.infer<typeof memoFormSchema>;
+
+export type MemoRecent = z.infer<typeof memoRecentSchema>;
+
+/** PARAMS */
+export const getMemoFormParamsSchema = z.object({
+  memoId: z.string().uuid('유효한 uuid가 아닙니다'),
+});
+
+/** RES for REQ */
+
+export const memoCreateResponseSchema = memoFormSchema.omit({
+  id: true,
+  linkedPortfolioInfo: true,
+});
+
+export const memoUpdateResponseSchema = memoFormSchema.omit({
+  linkedPortfolioInfo: true,
+});
+
+export const memoDeleteResponseSchema = memoFormSchema.pick({ id: true });
+
+export type MemoFormCreateResponse = z.infer<typeof memoCreateResponseSchema>;
+export type MemoFormUpdateResponse = z.infer<typeof memoUpdateResponseSchema>;
+export type MemoFormDeleteResponse = z.infer<typeof memoDeleteResponseSchema>;
+
+/** REQ */
+
+export const memoFormRequestSchema = z.discriminatedUnion('submitMode', [
+  z.object({
+    submitMode: z.literal('add'),
+    ...memoCreateResponseSchema.omit({}).shape,
+    linkedPortfolioId: actualPortfolioDetailedSchema.shape.id,
+  }),
+  z.object({
+    submitMode: z.literal('modify'),
+    ...memoUpdateResponseSchema.omit({}).shape,
+    linkedPortfolioId: actualPortfolioDetailedSchema.shape.id,
+  }),
+  z.object({
+    submitMode: z.literal('delete'),
+    ...memoDeleteResponseSchema.shape,
+  }),
+]);
+
+export type MemoFormRequest = z.infer<typeof memoFormRequestSchema>;
+
+export type MemoFormCreateRequest = Omit<
+  Extract<MemoFormRequest, { submitMode: 'add' }>,
+  'submitMode'
+>;
+export type MemoFormUpdateRequest = Omit<
+  Extract<MemoFormRequest, { submitMode: 'modify' }>,
+  'submitMode'
+>;
+export type MemoFormDeleteRequest = Omit<
+  Extract<MemoFormRequest, { submitMode: 'delete' }>,
+  'submitMode'
+>;
