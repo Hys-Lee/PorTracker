0ad56579d0e307a01f2b0f2cce728562c0468049
Hys-Lee <dltkdgus3275@gmail.com>
2026-01-27 17:42:58 +0900

    feat(FE-memos): 스키마 변경에 따른 타입 정리

--- apps/core/types/memos/referenceData.d.ts
+++ apps/core/types/memos/referenceData.d.ts
@@ -1,32 +1,8 @@
 import { MemoTypeValue, TransactionValue, CurrencyValue } from '../common';
-import {
-  ActualPortfolio,
-  ActualPortfolioDetail,
-  AllPortfolioDetail,
-  TargetPortfolioDetail,
-} from '@core/schemas/features/portfolios/portfolios.schema';
-
-export type PortfolioReferenceData =
-  //   | ActualPortfolioDetail
-  //   | TargetPortfolioDetail
-  AllPortfolioDetail | NonePortfolio;
+import type { AllPortfolioDetail } from '@core/schemas/features/memos/memos.schema';
+export type PortfolioReferenceData = AllPortfolioDetail | NonePortfolio;
 
 export interface NonePortfolio
   extends Record<keyof Pick<AllPortfolioDetail, 'portfolioType'>, string> {
   portfolioType: 'none';
 }
-
-/** Target Reference 관련 임시 (schema 나오기 전)*/
-
-// export type TargetPortfolio = TargetReferenceProps['targetsInfo'][number] & {
-//   id: string;
-// };
-
-// export interface TargetReferenceProps {
-//   targetsInfo: {
-//     assetName: string;
-//     assetType: string;
-//     currentRatioBps: number;
-//     ratioDeltaBps: number;
-//   }[];
-// }
