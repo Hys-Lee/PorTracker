4b4f6aaef627919a89fbba3cf6b6b4d9109bf6a4
Hys-Lee <dltkdgus3275@gmail.com>
2026-03-19 20:22:27 +0900

    feat(FE-api-layer): msw 개발환경에서 임시로 제거.

--- apps/core/instrumentation-client.ts
+++ apps/core/instrumentation-client.ts
@@ -4,8 +4,8 @@ const mswMocking = async () => {
   if (process.env.NODE_ENV !== 'production') {
     // 동적으로 server.ts를 import 합니다.
     const { enableMocking } = await import('@core/mocks');
-    enableMocking();
-    console.log('[MSW] Client-side mocking enabled.');
+    // enableMocking();
+    // console.log('[MSW] Client-side mocking enabled.');
   }
 };
 
--- apps/core/instrumentation.ts
+++ apps/core/instrumentation.ts
@@ -3,8 +3,8 @@ export async function register() {
     if (process.env.NODE_ENV !== 'production') {
       // 동적으로 server.ts를 import 합니다.
       const { enableMocking } = await import('./mocks');
-      enableMocking();
-      console.log('[MSW] Server-side mocking enabled.');
+      // enableMocking();
+      // console.log('[MSW] Server-side mocking enabled.');
     }
   }
 }
